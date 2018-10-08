7a31492b9e953dd9eb1deae4adfbee65a67f27f3
Brian Wahler <Brian@Brians-MacBook-Pro.local>
2018-10-08 16:40:41 -0400

    lab1

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lab_1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!");
+
+            do
+            {
+
+                Console.WriteLine("Please enter Length:");
+                String Length = Console.ReadLine();
+                double lengthRoom = double.Parse(Length);
+
+                Console.WriteLine("Please enter the width:");
+                String Width = Console.ReadLine();
+                double widthRoom = double.Parse(Width);
+
+                Console.WriteLine("Please enter the height:");
+                String Height = Console.ReadLine();
+                double heightRoom = double.Parse(Height);
+
+                double area = lengthRoom * widthRoom;
+                double perimeter = 2 * lengthRoom + 2 * widthRoom;
+                double volume = widthRoom * lengthRoom * heightRoom;
+                Console.WriteLine("Area: " + area);
+                Console.WriteLine("Perimeter: " + perimeter);
+                Console.WriteLine("Volume: " + volume);
+
+                Console.WriteLine("Continue (y/n)");
+
+            } 
+            //If anything other than a y is enter the program will exit
+            while (Console.ReadLine() == "y");
+        }
+    }
+}
